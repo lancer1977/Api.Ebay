7165999b1c8b38f01a847c8a7a8c70958824a00d
Christopher Richmond <lancer1977@gmail.com>
2026-02-22 17:58:38 -0500

    Missed?

---
+++ tests/PolyhydraGames.API.Ebay.Tests/AuthorizationCodeTokenResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PolyhydraGames.API.Ebay.Tests
+{
+    public sealed class AuthorizationCodeTokenResponse
+    {
+        [JsonPropertyName("access_token")] public string AccessToken { get; init; } = string.Empty;
+        [JsonPropertyName("expires_in")] public int ExpiresInSeconds { get; init; }
+        [JsonPropertyName("token_type")] public string TokenType { get; init; } = string.Empty; // "User Access Token"
+        [JsonPropertyName("refresh_token")] public string RefreshToken { get; init; } = string.Empty;
+        [JsonPropertyName("refresh_token_expires_in")] public int? RefreshTokenExpiresInSeconds { get; init; }
+        [JsonExtensionData] public Dictionary<string, object?> Extra { get; init; } = new();
+    }
+}
---
+++ tests/PolyhydraGames.API.Ebay.Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PolyhydraGames.API.Ebay.Tests
+{
+    internal sealed class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> _handler;
+
+        public FakeHttpMessageHandler(Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> handler)
+        {
+            _handler = handler;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_handler(request, cancellationToken));
+        }
+    }
+}
